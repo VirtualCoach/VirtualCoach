06d98b091fc49596b7865561f1787fd8e69d0128
Bryan Cuccioli <blc72@cornell.edu>
2011-11-05 16:05:20 -0400

    Added validation method

--- api/CSV_Handler.php
+++ api/CSV_Handler.php
@@ -5,6 +5,11 @@ class CSV_Handler {
 
   private $db_conn;
 
+  function __construct() {
+    # probably only want to instantiate this class to call validate in this case
+    $this->db_conn = null;
+  }
+
   function __construct($dbhost, $dbname, $dbuser, $dbpass) {
     # create the database connection
     $this->db_conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
@@ -12,8 +17,10 @@ class CSV_Handler {
     # handle connection failure
     if (mysqli_connect_errno()) {
       $db_conn = null;
-      die("Connect failed: ". mysqli_connect_error());
+      return -1;
     }
+    
+    return 0;
   }
   
   function __destruct() {
@@ -22,6 +29,11 @@ class CSV_Handler {
   }
   
   function parse_file($filename, $uid) {
+  
+    # make sure the db connection was initialized
+    if ($this->db_conn == null) {
+      return -1;
+    }
   
     $csv = new File_CSV_DataSource;
     $csv->load($filename);
@@ -38,8 +50,45 @@ class CSV_Handler {
       $speed = $data["kph"];
       $hr = $data["hr"];
       
-      $stmt->execute();
+      # return -1 if the sql query fails
+      if (!$stmt->execute()) {
+        return -1;
+      }
     }
+    
+    return 0; # got this far -> success!
+  }
+  
+  function validate($filename) {
+    
+    $fh = fopen($filename, "r");
+    if (!$fh) {
+      # file not found
+      return false;
+    }
+    
+    $had_data = false;
+    $line = fgets($fh);
+    $num_cols = count(preg_split(',', $line));
+        
+    while (($line = fgets($fh)) !== false) {
+      $had_data = true;
+      $data = preg_split(',', $line);
+      
+      # fail if not all lines have the same number of list elements
+      if (count($data) != $num_cols) {
+        return false;
+      }
+      
+      foreach (preg_split(',', $line) as $entry) {
+        if (!is_numeric($entry)) {
+          return false;
+        }
+      }
+    }
+    
+    fclose($fh);
+    return $had_data; # make sure we actually read something
   }
 }
 ?>
